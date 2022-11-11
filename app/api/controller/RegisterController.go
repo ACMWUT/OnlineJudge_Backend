94ef2b7db3c889a941e94fc3f5754c5c6e24a899
EZ4ZZW <zhiwayzhang@outlook.com>
2022-11-11 13:13:41 +0800

    feat(#3): 注册时校验当前全局配置

--- app/api/controller/RegisterController.go
+++ app/api/controller/RegisterController.go
@@ -5,11 +5,33 @@ import (
 	"OnlineJudge/app/common/validate"
 	"OnlineJudge/app/helper"
 	"OnlineJudge/constants"
-	"github.com/gin-gonic/gin"
+	"OnlineJudge/constants/redis_key"
+	"OnlineJudge/core/database"
 	"net/http"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gin-gonic/gin"
 )
 
 func Register(c *gin.Context) {
+
+	// Check Permission
+	redisStr := redis_key.RegisterPermission()
+	if value, err := database.GetFromRedis(redisStr); err == nil {
+		status, _ := redis.Int64(value, err)
+		if status == constants.PermissionDenied {
+			c.JSON(http.StatusOK, helper.ApiReturn(constants.CodeError, "当前不允许新用户注册", nil))
+			c.Abort()
+			return
+		}
+	} else {
+		// status not in Redis
+		configModel := model.GlobalConfig{}
+		// get register config
+		registerConfig := configModel.FindGlobalConfigByID(1)
+		_ = database.PutToRedis(redisStr, registerConfig.Status, 3600)
+	}
+
 	var userModel = model.User{}
 	var userValidate = validate.UserValidate
 	var userSubmitLogModel = model.UserSubmitLog{}
